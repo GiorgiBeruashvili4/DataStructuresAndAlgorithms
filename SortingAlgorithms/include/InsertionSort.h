d8dcce780d924b68fd99692db63da8446022796c
Giorgi Beruashvili <226286135+GiorgiBeruashvili4@users.noreply.github.com>
2026-07-29 13:08:21 +0400

    Implement Insertion Sort

---
+++ SortingAlgorithms/include/InsertionSort.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+
+// Insertion Sort
+// 
+// Time complexity:
+// Best case: O(n) -> If array is already sorted
+// Average case: O(n²)
+// Worst case: O(n²) -> If array is sorted in reverse order
+//
+// Space complexity: O(1)
+// Stable: Yes
+// In-place: Yes
+
+template<typename T>
+void InsertionSort(std::vector<T>& arr)
+{
+	const std::size_t size = arr.size();
+
+	for (std::size_t i = 1;i < size;++i)
+	{
+		T key = arr[i];
+		std::size_t j = i;
+
+		while (j > 0 && key < arr[j - 1])
+		{
+			arr[j] = arr[j - 1];
+			--j;
+		}
+
+		arr[j] = key;
+	}
+}
