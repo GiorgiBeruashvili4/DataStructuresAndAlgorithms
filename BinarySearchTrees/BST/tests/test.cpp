04604f54ed3a15853748683cf066df8c5d0387d5
Giorgi Beruashvili <226286135+GiorgiBeruashvili4@users.noreply.github.com>
2026-08-08 20:20:31 +0400

    Add CMake build system and Google Test for BST

---
+++ BinarySearchTrees/BST/tests/test.cpp
@@ -0,0 +1,149 @@
+#include <gtest/gtest.h>
+#include "BST.h"
+#include <sstream>
+#include <stdexcept>
+
+TEST(BSTTest, EmptyOnConstruction)
+{
+	BST<int> tree;
+	EXPECT_TRUE(tree.empty());
+	EXPECT_EQ(tree.size(), 0);
+}
+
+TEST(BSTTest, InsertAndContains)
+{
+	BST<int> tree;
+	tree.insertRecursive(10);
+	tree.insertRecursive(5);
+	tree.insertRecursive(15);
+
+	EXPECT_TRUE(tree.containsRecursive(10));
+	EXPECT_FALSE(tree.containsRecursive(100));
+}
+
+TEST(BSTTest, InOrderIsSorted)
+{
+	BST<int> tree;
+	for (int i : {10, 5, 15, 3, 7})
+	{
+		tree.insertIterative(i);
+	}
+
+	std::ostringstream out;
+	tree.inOrder(out);
+
+	EXPECT_EQ(out.str(), "3 5 7 10 15 ");
+}
+
+TEST(BSTTest, RemoveLeaf)
+{
+	BST<int> tree;
+	for (int i : {10, 5, 15})
+	{
+		tree.insertRecursive(i);
+	}
+
+	EXPECT_TRUE(tree.removeRecursive(5));
+	EXPECT_FALSE(tree.containsRecursive(5));
+}
+
+TEST(BSTTest, RemoveNodeWithTwoChildren)
+{
+	BST<int> tree;
+	for (int i : {10, 5, 15, 3})
+	{
+		tree.insertIterative(i);
+	}
+
+	EXPECT_TRUE(tree.removeRecursive(10));
+	EXPECT_FALSE(tree.containsRecursive(10));
+
+	std::ostringstream out;
+	tree.inOrder(out);
+
+	EXPECT_EQ(out.str(), "3 5 15 ");
+}
+
+TEST(BSTTest, RemovePredecessorWithChild)
+{
+    BST<int> tree;
+    for (int i : {10, 5, 15, 2, 7, 1})
+    {
+        tree.insertRecursive(i);
+    }
+
+    EXPECT_TRUE(tree.removeRecursive(10));
+
+    std::ostringstream out;
+    tree.inOrder(out);
+
+    EXPECT_EQ(out.str(), "1 2 5 7 15 ");
+}
+
+TEST(BSTTest, RemoveNonExistentValue)
+{
+    BST<int> tree;
+    tree.insertRecursive(10);
+
+    EXPECT_FALSE(tree.removeRecursive(999));
+    EXPECT_EQ(tree.size(), 1u);
+}
+
+TEST(BSTTest, MinMaxThrowOnEmpty)
+{
+    BST<int> tree;
+
+    EXPECT_THROW(tree.min(), std::runtime_error);
+    EXPECT_THROW(tree.max(), std::runtime_error);
+}
+
+TEST(BSTTest, MinMax)
+{
+    BST<int> tree;
+    for (int i : {10, 5, 15, 3, 20})
+    {
+        tree.insertRecursive(i);
+    }
+
+    EXPECT_EQ(tree.min(), 3);
+    EXPECT_EQ(tree.max(), 20);
+}
+
+TEST(BSTTest, CopyConstructorIsDeep)
+{
+    BST<int> a;
+    for (int i : {10, 5, 15})
+    {
+        a.insertRecursive(i);
+    }
+
+    BST<int> b(a);
+    b.insertRecursive(20);
+
+    EXPECT_FALSE(a.containsRecursive(20));
+}
+
+TEST(BSTTest, Equality)
+{
+    BST<int> a, b;
+    for (int i : {10, 5, 15})
+    {
+        a.insertRecursive(i);
+    }
+    for (int i : {10, 15, 5})
+    {
+        b.insertRecursive(i);
+    }
+
+    EXPECT_TRUE(a == b);
+}
+
+TEST(BSTTest, ClearEmptiesTree)
+{
+    BST<int> tree;
+
+    tree.insertRecursive(10);
+    tree.clear();
+
+    EXPECT_TRUE(tree.empty());
+}
