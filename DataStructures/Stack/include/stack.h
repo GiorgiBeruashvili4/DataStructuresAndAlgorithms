d76fb5aa2c3f12780c3143ed65d2f9e10a8105ad
Giorgi Beruashvili <226286135+GiorgiBeruashvili4@users.noreply.github.com>
2026-08-10 18:13:48 +0400

    Implement Stack class: push, pop, peek, empty, size, copy semantics

--- DataStructures/Stack/include/stack.h
+++ DataStructures/Stack/include/stack.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstddef>
+#include <stdexcept>
 
 template<typename T>
 class Stack
@@ -11,9 +12,6 @@ private:
 		Node* next;
 		Node(const T& value) : data(value), next(nullptr) {}
 	};
-
-	Node* topNode;
-	std::size_t stackSize;
 public:
 	Stack();
 	Stack(const Stack& other);
@@ -26,8 +24,144 @@ public:
 	void clear();
 
 	void push(const T& value);
-	bool pop();
+	void pop();
 
-	T& top();
-	const T& top() const;
+	T& peek();
+	const T& peek() const;
+private:
+	Node* top;
+	std::size_t stackSize;
 };
+
+template<typename T>
+inline Stack<T>::Stack() : top(nullptr), stackSize(0)
+{
+}
+
+template<typename T>
+inline Stack<T>::Stack(const Stack& other)
+	: top(nullptr), stackSize(0)
+{
+	if (this == &other || other.empty())
+	{
+		return;
+	}
+	
+	this->top = new Node(other.top->data);
+	Node* thisCurrent = this->top;
+	Node* otherCurrent = other.top->next;
+
+	while(otherCurrent !=nullptr)
+	{
+		thisCurrent->next = new Node(otherCurrent->data);
+		thisCurrent = thisCurrent->next;
+		otherCurrent = otherCurrent->next;
+	}
+
+	this->stackSize = other.stackSize;
+}
+
+template<typename T>
+inline Stack<T>::~Stack()
+{
+	clear();
+}
+
+template<typename T>
+inline Stack<T>& Stack<T>::operator=(const Stack& other)
+{
+	if (this != &other)
+	{
+		clear();
+
+		if (!other.empty())
+		{
+			top = new Node(other.top->data);
+			Node* thisCurrent = top;
+			Node* otherCurrent = other.top->next;
+
+			while (otherCurrent != nullptr)
+			{
+				thisCurrent->next = new Node(otherCurrent->data);
+				thisCurrent = thisCurrent->next;
+				otherCurrent = otherCurrent->next;
+			}
+
+			stackSize = other.stackSize;
+		}
+	}
+	
+	return *this;
+}
+
+template<typename T>
+inline bool Stack<T>::empty() const
+{
+	return stackSize == 0;
+}
+
+template<typename T>
+inline std::size_t Stack<T>::size() const
+{
+	return stackSize;
+}
+
+template<typename T>
+inline void Stack<T>::clear()
+{
+	Node* current = top;
+	while (current != nullptr)
+	{
+		Node* nextNode = current->next;
+		delete current;
+		current = nextNode;
+	}
+
+	top = nullptr;
+	stackSize = 0;
+}
+
+template<typename T>
+inline void Stack<T>::push(const T& value)
+{
+	Node* newNode = new Node(value);
+	newNode->next = top;
+	top = newNode;
+	++stackSize;
+}
+
+template<typename T>
+inline void Stack<T>::pop()
+{
+	if (empty())
+	{
+		throw std::out_of_range("Stack is empty");
+	}
+
+	Node* temp = top->next;
+	delete top;
+	top = temp;
+	--stackSize;
+}
+
+template<typename T>
+inline T& Stack<T>::peek()
+{
+	if (empty())
+	{
+		throw std::out_of_range("Stack is empty");
+	}
+
+	return top->data;
+}
+
+template<typename T>
+inline const T& Stack<T>::peek() const
+{
+	if (empty())
+	{
+		throw std::out_of_range("Stack is empty");
+	}
+
+	return top->data;
+}
