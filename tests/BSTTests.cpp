6535a944bd5aedb7aabbe6086bfb427d08d7376d
Giorgi Beruashvili <226286135+GiorgiBeruashvili4@users.noreply.github.com>
2026-08-15 17:28:11 +0400

    Update header file paths in test files

--- tests/BSTTests.cpp
+++ tests/BSTTests.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "BST.h"
+#include "dsa/BST.h"
 #include <sstream>
 #include <stdexcept>
 
--- tests/singlyLinkedListTests.cpp
+++ tests/singlyLinkedListTests.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "../include/dsa/singlyLinkedList.h"
+#include "dsa/singlyLinkedList.h"
 #include <stdexcept>
 
 TEST(SinglyLinkedListTest, PushBack)
