a1532217cd26ff26dbad42ea3050f3be1f929a24
Giorgi Beruashvili <226286135+GiorgiBeruashvili4@users.noreply.github.com>
2026-08-18 20:34:03 +0400

    Implement constructor, destructor, traversals, min/max, clear, empty, size

--- include/dsa/AVL.h
+++ include/dsa/AVL.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <ostream>
+#include <stdexcept>
 
 template<typename T>
 class AVL
@@ -65,3 +66,146 @@ private:
 	Node* findMin(Node* node) const;
 	Node* findMax(Node* node) const;
 };
+
+template<typename T>
+inline AVL<T>::AVL() : root(nullptr), count(0)
+{
+}
+
+template<typename T>
+inline AVL<T>::~AVL()
+{
+	clear();
+}
+
+template<typename T>
+inline std::size_t AVL<T>::size() const
+{
+	return count;
+}
+
+template<typename T>
+inline bool AVL<T>::empty() const
+{
+	return count == 0;
+}
+
+template<typename T>
+inline void AVL<T>::preOrder(std::ostream& out) const
+{
+	preOrder(out, root);
+}
+
+template<typename T>
+inline void AVL<T>::inOrder(std::ostream& out) const
+{
+	inOrder(out, root);
+}
+
+template<typename T>
+inline void AVL<T>::postOrder(std::ostream& out) const
+{
+	postOrder(out, root);
+}
+
+template<typename T>
+inline void AVL<T>::clear()
+{
+	clear(root);
+}
+
+template<typename T>
+inline const T& AVL<T>::max() const
+{
+	return findMax()->data;
+}
+
+template<typename T>
+inline const T& AVL<T>::min() const
+{
+	return findMin()->data;
+}
+
+template<typename T>
+inline void AVL<T>::preOrder(std::ostream& out, Node* node) const
+{
+	if (node == nullptr)
+	{
+		return;
+	}
+
+	out << node->data << " ";
+	preOrder(out, node->left);
+	preOrder(out, node->right);
+}
+
+template<typename T>
+inline void AVL<T>::inOrder(std::ostream& out, Node* node) const
+{
+	if (node == nullptr)
+	{
+		return;
+	}
+
+	preOrder(out, node->left);
+	out << node->data << " ";
+	preOrder(out, node->right);
+}
+
+template<typename T>
+inline void AVL<T>::postOrder(std::ostream& out, Node* node) const
+{
+	if (node == nullptr)
+	{
+		return;
+	}
+
+	preOrder(out, node->left);
+	preOrder(out, node->right);
+	out << node->data << " ";
+}
+
+template<typename T>
+inline void AVL<T>::clear(Node* node)
+{
+	if (node == nullptr)
+	{
+		return;
+	}
+
+	clear(node->left);
+	clear(node->right);
+	delete node;
+}
+
+template<typename T>
+inline AVL<T>::Node* AVL<T>::findMin(Node* node) const
+{
+	if (node == nullptr)
+	{
+		return nullptr;
+	}
+	
+	while (node->left != nullptr)
+	{
+		node = node->left;
+	}
+
+	return node;
+}
+
+template<typename T>
+inline AVL<T>::Node* AVL<T>::findMax(Node* node) const
+{
+	if (node == nullptr)
+	{
+		return nullptr;
+	}
+
+	while (node->right != nullptr)
+	{
+		node = node->right;
+	}
+
+	return node;
+}
